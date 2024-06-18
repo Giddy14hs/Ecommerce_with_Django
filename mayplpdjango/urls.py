30f87ace8165c5555114599ec70df3367d502777
lemis <lemisogideon@gmail.com>
2024-06-18 12:26:24 +0300

    Created the Django project

--- mayplpdjango/urls.py
+++ mayplpdjango/urls.py
@@ -16,10 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from plpecommerce import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('plpecommerce/', include('plpecommerce.urls')),
-    path('product_list', views.product_list, name='product_list'),
 ]
